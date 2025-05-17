8a887a671a9b01bb7d366068e938a3ac7943b09a
ttibsi <ashisbitt@icloud.com>
2025-05-17 16:47:51 +0100

    properly parse hello world!

--- src/lex.cpp
+++ src/lex.cpp
@@ -30,6 +30,7 @@ std::string token_to_str(Token_type tok) {
 
 // TODO: Escaped literals
 Lexer::Lexer(const std::string& contents) {
+    Tokens.reserve(16);
     tokenizer(contents);
 }
 
@@ -45,49 +46,63 @@ void Lexer::tokenizer(const std::string& contents) {
             switch (c) {
                 case '{':
                     Tokens.emplace_back(Token_type::LBRACE, std::string(1, c));
-                    break;
+                    text = "";
+                    continue;
                 case '}':
                     Tokens.emplace_back(Token_type::RBRACE, std::string(1, c));
-                    break;
+                    text = "";
+                    continue;
                 case ':':
                     Tokens.emplace_back(Token_type::COLON, std::string(1, c));
-                    break;
+                    text = "";
+                    continue;
                 case ';':
                     Tokens.emplace_back(Token_type::SEMI, std::string(1, c));
-                    break;
+                    text = "";
+                    continue;
                 case '[':
                     Tokens.emplace_back(Token_type::LBRACKET, std::string(1, c));
-                    break;
+                    text = "";
+                    continue;
                 case ']':
                     Tokens.emplace_back(Token_type::RBRACKET, std::string(1, c));
-                    break;
+                    text = "";
+                    continue;
                 case '(':
-                    Tokens.emplace_back(Token_type::FUNCTION, text.substr(0, text.size() - 1));
+                    if (is_builtin(text.substr(0, text.size() - 1))) {
+                        Tokens.emplace_back(Token_type::BUILTIN, text.substr(0, text.size() - 1));
+                    } else {
+                        Tokens.emplace_back(Token_type::FUNCTION, text.substr(0, text.size() - 1));
+                    }
+
                     Tokens.emplace_back(Token_type::LPAREN, std::string(1, c));
-                    break;
+                    text = "";
+                    continue;
                 case ')':
                     Tokens.emplace_back(Token_type::RPAREN, std::string(1, c));
-                    break;
+                    text = "";
+                    continue;
                 case '"':
                     in_string = true;
-                    break;
+                    continue;
             };
-
-            text = "";
-            continue;
         }
 
         if (c == '"') {
             Tokens.emplace_back(Token_type::STRING_LITERAL, text);
+            text = "";
+            in_string = false;
             continue;
         }
 
         if (std::isspace(c)) {
-            text = text.substr(text.size() - 1);
+            if (in_string) {
+                continue;
+            }
+
+            text = text.substr(0, text.size() - 1);
             if (std::find(keywords.begin(), keywords.end(), text) != keywords.end()) {
                 Tokens.emplace_back(Token_type::KEYWORD, text);
-            } else if (std::find(builtins.begin(), builtins.end(), text) != builtins.end()) {
-                Tokens.emplace_back(Token_type::BUILTIN, text);
             } else if (is_number(text)) {
                 Tokens.emplace_back(Token_type::NUMERIC_LITERAL, text);
             }
@@ -101,4 +116,8 @@ void Lexer::tokenizer(const std::string& contents) {
 [[nodiscard]] bool Lexer::is_number(const std::string& text) {
     std::regex pattern(R"(^\d+$|^0o[0-7]+$|^0b[0-1]+$|^0x[\dabcdefABCDEF]+$)");
     return std::regex_match(text, pattern);
+}
+
+[[nodiscard]] bool Lexer::is_builtin(const std::string& text) {
+    return std::find(builtins.begin(), builtins.end(), text) != builtins.end();
 }
